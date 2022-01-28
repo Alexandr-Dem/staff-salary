78d25b0dd5b75b391fb2f877980fdd00a0e6fe8e
Alexander Dementyev <APDementyev@sberbank.ru>
2022-01-28 12:05:01 +0300

    init commit

---
+++ src/main/java/dem/alex/staffsalary/StaffSalaryApplication.java
@@ -0,0 +1,13 @@
+package dem.alex.staffsalary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffSalaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffSalaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dem/alex/staffsalary/StaffSalaryApplicationTests.java
@@ -0,0 +1,13 @@
+package dem.alex.staffsalary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffSalaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
